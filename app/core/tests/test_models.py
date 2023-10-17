31f92fb61a63c46b113af6add33b264243723d44
AD\mojcal <mojca.lorber@domel.com>
2023-10-17 15:10:00 +0200

    Fixed typo

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -44,4 +44,4 @@ class ModelTests(TestCase):
             'test123',
         )
         self.assertTrue(user.is_superuser)
-        self.assertTrue(user.is_stuff)
+        self.assertTrue(user.is_staff)
